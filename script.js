3c49767fcdc86628f5d5521508d9b7d54721d8f0
LeSwift <103843513+tomh451@users.noreply.github.com>
2026-05-21 00:11:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const WEBHOOK_URL='PASTE_DISCORD_WEBHOOK';
+const WEBHOOK_URL='https://discord.com/api/webhooks/1506796423491096606/7lHq-4Z-JefJVIbgSqpJKjPrBCpv5z3_gKcXuPcBEotFBTy9AGQwqIX4luINNXckEXSA';
 document.getElementById('form').addEventListener('submit',async(e)=>{e.preventDefault();alert('Replace webhook in script.js');});
